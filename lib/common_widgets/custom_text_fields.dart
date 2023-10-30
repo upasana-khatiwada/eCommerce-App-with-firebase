8c685d02b1f6f51ac4f22082126c2748eeb76a6b
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-10-30 22:40:11 +0545

    textfield added login form

--- lib/common_widgets/custom_text_fields.dart
+++ lib/common_widgets/custom_text_fields.dart
@@ -1,11 +1,38 @@
+import 'package:ecommerce_app_with_firebase/consts/colors.dart';
+import 'package:ecommerce_app_with_firebase/consts/strings.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
-Widget customTextField({String? hint, String? title, controller, isPass}){
+Widget customTextField({String? hint, String? title, controller, isPass}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      
+      email.text.color(bermudaGrey).fontFamily("sans_bold").size(21).make(),
+      5.heightBox,
+      TextFormField(
+        obscureText: isPass,
+        controller: controller,
+        decoration: const InputDecoration(
+          hintStyle: TextStyle(
+            fontFamily: "sans_semibold",
+            color: textfieldGrey,
+          ),
+          hintText: emailHint,
+          isDense: true, //to make it little small
+          fillColor: lightGrey,
+          filled: true,
+          border: InputBorder.none,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: bermudaGrey,
+            ),
+          ),
+        ),
+      ),
+      5.heightBox,
     ],
   );
-
 }
+
+
+//
--- lib/views/auth_screen/login_screen.dart
+++ lib/views/auth_screen/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:ecommerce_app_with_firebase/common_widgets/app_logo.dart';
-import 'package:ecommerce_app_with_firebase/common_widgets/bg_widget.dart';
+import 'package:ecommerce_app_with_firebase/common_widgets/custom_text_fields.dart';
 import 'package:ecommerce_app_with_firebase/consts/colors.dart';
-import 'package:ecommerce_app_with_firebase/consts/images.dart';
 import 'package:ecommerce_app_with_firebase/consts/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -11,40 +10,67 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          backgroundColor: bermudaGrey,
-          floating: true,
-          //pinned: true,
-          //snap: true,
-          expandedHeight: MediaQuery.of(context).size.height * 0.25,
-          flexibleSpace: FlexibleSpaceBar(
-            background: Stack(children: [
-              Center(child: appLogoWidget()),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  //height: MediaQuery.of(context).size.height * 0.75,
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: bermudaGrey,
+            floating: true,
+            //pinned: true,
+            //snap: true,
+            expandedHeight: MediaQuery.of(context).size.height * 0.25,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Stack(
+                children: [
+                  Center(child: appLogoWidget()),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      //height: MediaQuery.of(context).size.height * 0.75,
 
-                  child: "Log in to $appname"
-                      .text
-                      .fontFamily("sans_bold")
-                      .white
-                      .size(22)
-                      .make(),
-                ),
-              )
-            ]),
+                      child: "Log in to $appname"
+                          .text
+                          .fontFamily("sans_bold")
+                          .white
+                          .size(22)
+                          .make(),
+                    ),
+                  ),
+                  50.heightBox,
+                ],
+              ),
+            ),
           ),
-        ),
-        SliverToBoxAdapter(
-          child: Container(
-            color: whiteColor,
-            height: MediaQuery.of(context).size.height*0.75,
-          ),
-        )
-      ],
+          SliverToBoxAdapter(
+            child: Container(
+                color: whiteColor,
+                height: MediaQuery.of(context).size.height * 0.75,
+                child: Column(
+                  children: [
+                    30.heightBox,
+
+                    //(context.screenHeight*0.1).heightBox;
+                    Container(
+                      child: Column(children: [
+                        customTextField(hint: emailHint,title: email,isPass: false),
+                        customTextField(hint: passwordHint, title : password,isPass: true),
+                      ])
+                          .box
+                          .color(const Color.fromARGB(255, 244, 244, 244))
+                          .rounded
+                          .padding(const EdgeInsets.all(16))
+                          .width(context.screenWidth - 70)
+                          .make(),
+                    )
+                  ],
+                )),
+          )
+        ],
+      ),
     );
   }
 }
+
+
+//,controller: controller.emailController
+//, controller: passwordController
