26d8e6424c994fb7c3686528f216c8a05ddb43cc
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-11-28 10:22:26 +0545

    order details added and items added on cart cannot be zero fixed

--- lib/views/orders_screen/components/order_place_details.dart
+++ lib/views/orders_screen/components/order_place_details.dart
@@ -3,23 +3,30 @@ import 'package:ecommerce_app_with_firebase/consts/strings.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-Widget orderPlaceDetails({data,}){
+Widget orderPlaceDetails({title1, title2, d1, d2}) {
   return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 8),
-              child: Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(children: [
-                    "Order Code ".text.fontFamily(semibold).make(),
-                    "${data['order_code']}".text.color(bermudaGrey).fontFamily(semibold).make(),
-                  ],),
-                  Column(
-                   children: [
-                    "Shipping method".text.fontFamily(semibold).make(),
-                    "${data['shipping_method']}".text.make(),
-                   ],
-                  )
-                ],
-              ),
-            );
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            "$title1 ".text.fontFamily(semibold).make(),
+            "$d1".text.color(bermudaGrey).fontFamily(semibold).make(),
+          ],
+        ),
+        SizedBox(
+          width: 156,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "$title2".text.fontFamily(semibold).make(),
+              "$d2".text.make(),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
 }
--- lib/views/orders_screen/orders_details.dart
+++ lib/views/orders_screen/orders_details.dart
@@ -1,8 +1,10 @@
 import 'package:ecommerce_app_with_firebase/consts/colors.dart';
 import 'package:ecommerce_app_with_firebase/consts/strings.dart';
+import 'package:ecommerce_app_with_firebase/views/orders_screen/components/order_place_details.dart';
 import 'package:ecommerce_app_with_firebase/views/orders_screen/components/order_status.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:intl/intl.dart' as intl;
 
 class OrdersDetails extends StatelessWidget {
   final dynamic data;
@@ -21,32 +23,134 @@ class OrdersDetails extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            orderStatus(
-                color: redColor,
-                icon: Icons.done,
-                title: "Placed",
-                showDone: data['order_placed']),
-            orderStatus(
-                color: Colors.blue,
-                icon: Icons.thumb_up,
-                title: "Confirmed",
-                showDone: data['order_confirmed']),
-            orderStatus(
-                color: Colors.yellow,
-                icon: Icons.car_crash,
-                title: "Delivery",
-                showDone: data['order_on_delivery']),
-            orderStatus(
-                color: Colors.purple,
-                icon: Icons.done_all_rounded,
-                title: "Delivered",
-                showDone: data['order_delivered']),
-            Divider(),
-            10.heightBox,
-            
-          ],
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              orderStatus(
+                  color: redColor,
+                  icon: Icons.done,
+                  title: "Placed",
+                  showDone: data['order_placed']),
+              orderStatus(
+                  color: Colors.blue,
+                  icon: Icons.thumb_up,
+                  title: "Confirmed",
+                  showDone: data['order_confirmed']),
+              orderStatus(
+                  color: Colors.yellow,
+                  icon: Icons.car_crash,
+                  title: "Delivery",
+                  showDone: data['order_on_delivery']),
+              orderStatus(
+                  color: Colors.purple,
+                  icon: Icons.done_all_rounded,
+                  title: "Delivered",
+                  showDone: data['order_delivered']),
+              const Divider(),
+              10.heightBox,
+              Column(
+                children: [
+                  orderPlaceDetails(
+                    d1: data['order_code'],
+                    d2: data['shipping_method'],
+                    title1: "Order Code",
+                    title2: "Shipping Method",
+                  ),
+                  orderPlaceDetails(
+                    d1: intl.DateFormat()
+                        .add_yMd()
+                        .format((data['order_date'].toDate())),
+                    d2: data['payment_method'],
+                    title1: "Order Date",
+                    title2: "Payment Method",
+                  ),
+                  orderPlaceDetails(
+                    d1: "Unpaid",
+                    d2: "Order Placed",
+                    title1: "Payment Status",
+                    title2: "Delivery Status",
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            "Shipping Address".text.fontFamily(semibold).make(),
+                            "${data['order_by_name']}".text.make(),
+                            "${data['order_by_email']}".text.make(),
+                            "${data['order_by_address']}".text.make(),
+                            "${data['order_by_city']}".text.make(),
+                            "${data['order_by_state']}".text.make(),
+                            "${data['order_by_phone']}".text.make(),
+                            "${data['order_by_postalcode']}".text.make(),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 130,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              "Total Amount".text.fontFamily(semibold).make(),
+                              "${data['total_amount']}"
+                                  .text
+                                  .color(bermudaGrey)
+                                  .fontFamily(fontBold)
+                                  .make(),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ).box.outerShadowMd.white.make(),
+              const Divider(),
+              10.heightBox,
+              "Ordered Product"
+                  .text
+                  .size(16)
+                  .color(darkFontGrey)
+                  .fontFamily(semibold)
+                  .makeCentered(),
+              10.heightBox,
+              ListView(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                children: List.generate(data['orders'].length, (index) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      orderPlaceDetails(
+                          title1: data['orders'][index]['title'],
+                          title2: data['orders'][index]['tprice'],
+                          d1: "${data['orders'][index]['qty']}x",
+                          d2: "Refundable"),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Container(
+                          width: 30,
+                          height: 20,
+                          //providing a color of the product
+                          color: Color(
+                            int.parse(data['orders'][index]['color']),
+                          ),
+                        ),
+                      ),
+                      const Divider(),
+                    ],
+                  );
+                }).toList(),
+              ).box.outerShadowMd.white.margin(const EdgeInsets.only(bottom: 4)).makeCentered(),
+               20.heightBox,
+               
+            ],
+          ),
         ),
       ),
     );
