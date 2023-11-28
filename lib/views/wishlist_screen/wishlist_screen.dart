1ba3347b8cf3a5621074497cab6417be4ec8895f
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2023-11-28 10:49:11 +0545

    added wishlist

--- lib/views/wishlist_screen/wishlist_screen.dart
+++ lib/views/wishlist_screen/wishlist_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_app_with_firebase/common_widgets/loading_indicator.dart';
 import 'package:ecommerce_app_with_firebase/consts/colors.dart';
+import 'package:ecommerce_app_with_firebase/consts/firebase_consts.dart';
 import 'package:ecommerce_app_with_firebase/consts/strings.dart';
 import 'package:ecommerce_app_with_firebase/services/firestore_services.dart';
 import 'package:flutter/material.dart';
@@ -12,26 +13,65 @@ class WishlistScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: whiteColor,
+      backgroundColor: whiteColor,
       appBar: AppBar(
-        title: "My Wishlist".text.color(darkFontGrey).fontFamily(semibold).make(),
+        title:
+            "My Wishlist".text.color(darkFontGrey).fontFamily(semibold).make(),
       ),
-      body: StreamBuilder(stream: FirestoreServices.getWishlists(),
-       builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot) {
-         if(!snapshot.hasData){
+      body: StreamBuilder(
+          stream: FirestoreServices.getWishlists(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
               return Center(
                 child: loadingIndicator(),
               );
-            }else if(snapshot.data!.docs.isEmpty){
+            } else if (snapshot.data!.docs.isEmpty) {
               return "No orders yet!".text.color(darkFontGrey).makeCentered();
-            }else{
+            } else {
+              var data = snapshot.data!.docs;
               return Column(
                 children: [
-
+                  Expanded(
+                      child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              leading: Image.network(
+                                "${data[index]['p_imgs'][0]}",
+                                width: 80,
+                                fit: BoxFit.cover,
+                              ),
+                              title: "${data[index]['p_name']}"
+                                  .text
+                                  .fontFamily(semibold)
+                                  .size(16)
+                                  .make(),
+                              subtitle: "${data[index]['p_price']}"
+                                  .numCurrency
+                                  .text
+                                  .color(bermudaGrey)
+                                  .fontFamily(semibold)
+                                  .make(),
+                              trailing: const Icon(
+                                Icons.favorite,
+                                color: redColor,
+                              ).onTap(() async {
+                                await firestore
+                                    .collection(productsCollection)
+                                    .doc(data[index].id)
+                                    .set({
+                                  'p_wishlist':
+                                      FieldValue.arrayRemove([currentUser!.uid])
+                                }, SetOptions(merge: true));
+                              }),
+                            );
+                          })),
                 ],
               );
             }
-       }),
+          }),
     );
   }
 }
